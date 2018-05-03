44287d9d720fe936d0b5b7dde00684aea1cf909d
Michiel Vanthoor <michielvanthoor@gmail.com>
2018-05-03 20:45:43 +0200

    Add boiler python code for GCS

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 # Imports the Google Cloud client library
 from google.cloud import storage
 
-# Download GCS Blop files locally
-client = storage.Client()
-bucket = client.get_bucket('20180503sr')
-blob_name = 'G0023157.JPG'
-blob = storage.Blob(blob_name, bucket)
-with open('staging/'+blob_name, 'wb') as file_obj:
-    blob.download_to_file(file_obj)
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+def dowload_blobs_in_bucket(bucket_name):
+    """Download all the blobs in the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blobs = bucket.list_blobs()
+
+    for blob in blobs:
+        download_blob(bucket_name, blob, blo)
+
+if __name__ == "__main__":
+    # Download GCS Blop files locally
+    client = storage.Client()
+    bucket = client.get_bucket('20180503sr')
+    list_blobs()
+    blob_name = 'G0023157.JPG'
+    blob = storage.Blob(blob_name, bucket)
+    with open('staging/'+blob_name, 'wb') as file_obj:
+        blob.download_to_file(file_obj)
 
 # Creates the new bucket
 #storage_client = storage.Client()
