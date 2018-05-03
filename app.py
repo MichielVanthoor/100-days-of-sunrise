ee05c7ac688ea5473d8d79d501d3560dbc53580e
Michiel Vanthoor <michielvanthoor@gmail.com>
2018-05-03 21:00:21 +0200

    Add boiler python code for GCS

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(source_blob_name)
-    blob.download_to_file(destination_file_name)
+    blob.download_to_filename(destination_file_name)
 
 def dowload_blobs_in_bucket(bucket_name):
     """Download all the blobs in the bucket."""
