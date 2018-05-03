01574d72981aeb28b353cd00211f5b47ddec8bac
Michiel Vanthoor <michielvanthoor@gmail.com>
2018-05-03 19:43:10 +0200

    Add boiler python code for GCS

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 # Imports the Google Cloud client library
-from google.cloud import storage
+from google.cloud import storage, Blob
 
 # Instantiates a client
 storage_client = storage.Client()
-
-# The name for the new bucket
-bucket_name = 'my-new-bucket'
+bucket = client.get_bucket('100-days-of-sunrise')
+blob = Blob('G0023157.JPG', bucket)
+with open('/staging', 'wb') as file_obj:
+    blob.download_to_file(file_obj)
 
 # Creates the new bucket
-bucket = storage_client.create_bucket(bucket_name)
+#bucket_name = '100-days-of-sunrise'
+#bucket = storage_client.create_bucket(bucket_name)
+#print('Bucket {} created.'.format(bucket.name))
 
-print('Bucket {} created.'.format(bucket.name))
+# Download GCS Blop files locally
