5750479b36ef7fbc7a6c72d28764b66c41f5ca2d
Michiel Vanthoor <michielvanthoor@gmail.com>
2018-05-03 20:59:19 +0200

    Add boiler python code for GCS

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def dowload_blobs_in_bucket(bucket_name):
     blobs = bucket.list_blobs()
 
     for blob in blobs:
-        download_blob(bucket_name, blob, blob)
+        download_blob(bucket_name, blob.name, blob.name)
 
 if __name__ == "__main__":
     # Download GCS Blop files locally
