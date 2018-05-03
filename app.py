6befcdad3af5717e83bef151d31e91393c4238b8
Michiel Vanthoor <michielvanthoor@gmail.com>
2018-05-03 22:07:17 +0200

    Add the upload functionality

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-# Imports the Google Cloud client library
 from google.cloud import storage
+import os
 
 def download_blob(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(source_blob_name)
-    blob.download_to_filename('staging/'+destination_file_name)
+    blob.download_to_filename(destination_file_name)
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print('File {} uploaded to {}.'.format(
+        source_file_name,
+        destination_blob_name))
 
 def dowload_blobs_in_bucket(bucket_name):
     """Download all the blobs in the bucket."""
@@ -15,11 +27,22 @@ def dowload_blobs_in_bucket(bucket_name):
     blobs = bucket.list_blobs()
 
     for blob in blobs:
-        download_blob(bucket_name, blob.name, blob.name)
+        download_blob(bucket_name, blob.name, 'staging/'+bucket_name+'/'+blob.name)
 
 if __name__ == "__main__":
-    # Download GCS Blop files locally
-    dowload_blobs_in_bucket('20180503sr')
+    bucket = '20180503sr'
+
+    # Download the images from GCS locally
+    dowload_blobs_in_bucket(bucket)
+
+    # Create the timelapse
+    video_name = 'sunrise.mp4'
+    timelapse_command = 'ffmpeg -r 15 -start_number 23157 -i G00%d.JPG -s 1280x720 -vcodec libx264 '+video_name
+    os.system(timelapse_command)
+
+    # Upload the timelaps video to GCS
+    upload_blob(bucket,'staging/'+bucket_name+'/'+video_name,video_name)
+
 
 # Creates the new bucket
 #storage_client = storage.Client()
