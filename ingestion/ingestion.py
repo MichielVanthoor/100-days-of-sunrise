7f0fc652462dce4def79414f4a2f8483011195af
Michiel Vanthoor <michielvanthoor@gmail.com>
2018-05-06 08:52:12 +0200

    Implement timelapse video mode on ingestion

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -1,21 +1,26 @@
 import urllib2
 from time import sleep
 
+def switch_to_SSID(ssid):
+    return
+
 def start_timelapse():
     # Make sure to be connected to right Wifi & GoPro is on!
 
     # Go to time lapse mode
-    urllib2.urlopen('http://10.5.5.9/camera/CM?t=17461234&p=%03')
+    urllib2.urlopen('http://10.5.5.9/gp/gpControl/command/sub_mode?mode=0&sub_mode=1')
 
     # Start the time lapse recording
-    sleep(3)
-    urllib2.urlopen('http://10.5.5.9/bacpac/SH?t=17461234&p=%01')
-
+    sleep(5)
+    # Set interval fime
+    urllib2.urlopen('http://10.5.5.9/gp/gpControl/setting/5/6')
+    sleep(1)
+    urllib2.urlopen('http://10.5.5.9/gp/gpControl/command/shutter?p=1')
 
 def stop_timelapse():
     # Make sure to be connected to right Wifi!
     # Stop the time lapse recording
-    urllib2.urlopen('http://10.5.5.9/bacpac/SH?t=17461234&p=%00')
+    urllib2.urlopen('http://10.5.5.9/gp/gpControl/command/shutter?p=0')
 
 def dowload_files(url):
     urllib.urlretrieve('http://site.com/', filename='filez.txt')
@@ -24,11 +29,3 @@ if __name__ == "__main__":
     start_timelapse()
     sleep(10)
     stop_timelapse()
-
-
-### Wasteland
-    # Turn GoPro on
-    #urllib2.urlopen('http://10.5.5.9/bacpac/PW?t=17461234&p=%01')
-
-    # Turn GoPro off
-    #urllib2.urlopen('http://10.5.5.9/bacpac/PW?t=17461234&p=%00')
