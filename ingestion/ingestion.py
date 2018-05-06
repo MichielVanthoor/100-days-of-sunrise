3d9c90edf3b5be0ea9959bd7a579c40ec904b11d
Michiel Vanthoor <michielvanthoor@MacBookvanFrank.lan>
2018-05-06 16:47:38 +0200

    Add dowloading function to ingestion

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -1,12 +1,10 @@
-import urllib2
+import urllib2, re
 from time import sleep
 
 def switch_to_SSID(ssid):
     return
 
 def start_timelapse():
-    # Make sure to be connected to right Wifi & GoPro is on!
-
     # Go to time lapse mode
     urllib2.urlopen('http://10.5.5.9/gp/gpControl/command/sub_mode?mode=0&sub_mode=1')
 
@@ -18,14 +16,39 @@ def start_timelapse():
     urllib2.urlopen('http://10.5.5.9/gp/gpControl/command/shutter?p=1')
 
 def stop_timelapse():
-    # Make sure to be connected to right Wifi!
     # Stop the time lapse recording
     urllib2.urlopen('http://10.5.5.9/gp/gpControl/command/shutter?p=0')
 
-def dowload_files(url):
-    urllib.urlretrieve('http://site.com/', filename='filez.txt')
+def dowload_latest_file(filename):
+    # Get URL of latest video
+    video_server_url = 'http://10.5.5.9:8080/videos/DCIM/100GOPRO/'
+    response = urllib2.urlopen(video_server_url)
+    html = response.read()
+
+    pattern = re.compile("^.*\.MP4$")
+    pattern.match(string)
+
+
+    video_name = 'GOPR3368.MP4'
+    url = video_server_url+video_name
+
+    # Save file in local directory
+    file = urllib2.urlopen(url)
+    data = file.read()
+    with open(filename, "wb") as code:
+      code.write(data)
+
+def upload_to_gcs(filename):
+    return
 
 if __name__ == "__main__":
+    # Make sure to be connected to right Wifi!
+
+    #switch_to_SSID('goprohotspot') 
     start_timelapse()
     sleep(10)
     stop_timelapse()
+    sleep(5)
+    dowload_latest_file('date_sunrise.mp4')
+    #switch_to_SSID('goprohotspot') 
+
